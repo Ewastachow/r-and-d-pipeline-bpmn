37eca8d5bcf7fa9d9f2eb076de3a5fbd0778b9ea
Ewa Stachow <ewa.stachow@sabre.com>
2020-05-22 18:28:21 +0200

    Add dummy endpoint

--- src/main/java/pl/edu/agh/kis/randdpipelinebpmn/RAndDPipelineBpmnApplication.java
+++ src/main/java/pl/edu/agh/kis/randdpipelinebpmn/RAndDPipelineBpmnApplication.java
@@ -2,12 +2,26 @@ package pl.edu.agh.kis.randdpipelinebpmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
+@Configuration
 @SpringBootApplication
 public class RAndDPipelineBpmnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RAndDPipelineBpmnApplication.class, args);
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> route() {
+		return RouterFunctions.route(RequestPredicates.GET("/R-and-D"),
+				rq -> ServerResponse.ok().body(BodyInserters.fromValue("Welcome in BPMN in pipeline!")));
 	}
 
 }
