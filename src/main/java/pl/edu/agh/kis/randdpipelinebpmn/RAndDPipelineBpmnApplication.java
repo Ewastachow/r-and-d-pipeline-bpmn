7a72719e7fa19f1d4bebff1a39e3cc6290000bae
Ewa Stachow <ewa.stachow@sabre.com>
2020-05-22 18:10:49 +0200

    Init by Spring Initializr

---
+++ src/main/java/pl/edu/agh/kis/randdpipelinebpmn/RAndDPipelineBpmnApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.kis.randdpipelinebpmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RAndDPipelineBpmnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RAndDPipelineBpmnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/agh/kis/randdpipelinebpmn/RAndDPipelineBpmnApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.kis.randdpipelinebpmn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RAndDPipelineBpmnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
