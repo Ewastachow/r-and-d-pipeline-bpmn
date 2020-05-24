90f03c39d9c594ed00056fece1fd966812c73f2c
Ewa Stachów <stachow.ewa@gmail.com>
2020-05-24 11:51:09 +0200

    [TMP] Maven project build added

--- r-and-d-service/.jenkins/Jenkinsfile.groovy
+++ r-and-d-service/.jenkins/Jenkinsfile.groovy
@@ -2,10 +2,24 @@
 
 pipeline {
     agent any
+    tools {
+        jdk 'jdk8'
+        maven 'maven3'
+    }
     stages {
         stage('hello world') {
             steps {
                 echo "hello world"
+            }
+        }
+        stage('install') {
+            steps {
+                sh 'mvn clean install'
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: 'r-and-d-service/target/process-test-coverage/*/*.html', onlyIfSuccessful: true
+                }
             }
         }
     }
